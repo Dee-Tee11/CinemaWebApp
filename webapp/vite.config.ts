a61197dcdf50ea0c31117142de365622353601a6
Diogo <dteixeira@aldar.com>
2025-09-28 16:03:48 +0100

    Removing bugs

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -1,7 +1,6 @@
 import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,7 +10,6 @@ export default defineConfig({
         plugins: [["babel-plugin-react-compiler"]],
       },
     }),
-    tailwindcss(),
   ],
   resolve: {
     alias: {
